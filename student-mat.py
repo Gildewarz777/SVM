0f580e6834c3bc0fff06d7dab1f4a393c8ddc722
Kyota Lannelongue <kyota95@live.fr>
2017-10-10 23:27:38 +0200

    student-mat

--- student-mat.py
+++ student-mat.py
@@ -57,6 +57,7 @@ u = np.random.randint(0, 3, dataset.shape[0])
 dataset_train = dataset[u != 0]
 dataset_test = dataset[u == 0]
 
+
 x0 = dataset_train.absences
 x1 = dataset_train.freetime
 x2 = dataset_train.age
@@ -76,43 +77,23 @@ x_test = pd.concat([x0, x1, x2, x3, x4, x5], axis = 1)
 print(x_train.shape)
 print(x_test.shape)
 
-y_train = dataset_train.health
-y_test = dataset_test.health
-print(y_train.shape)
-print(y_test.shape)
 
-for fig_num, kernel in enumerate(('linear', 'rbf')):
+y_train = np.ravel(pd.get_dummies(dataset_train.schoolsup, drop_first=True))
+y_test = np.ravel(pd.get_dummies(dataset_test.schoolsup, drop_first=True))
+
+print("y_train shape ", y_train.shape)
+print("y_test shape ", y_test.shape)
+
+for fig_num, kernel in enumerate(('linear', 'rbf', 'poly')):
     print("training new model")
-    clf = svm.SVC(kernel=kernel, gamma=0.01, C=1)
+    clf = svm.SVC(kernel=kernel, gamma=0.01, C=100)
     clf.fit(x_train, y_train)
     score = clf.score(x_test, y_test)
-    print(score)
+    print("Score", score)
 
-models = (svm.SVC(kernel='linear', C=C), svm.LinearSVC(C=C),
-          svm.SVC(kernel='rbf', gamma=0.7, C=C),
-          svm.SVC(kernel='poly', degree=3, C=C))
+"""models = (svm.SVC(kernel='linear', C=C), svm.LinearSVC(C=C), svm.SVC(kernel='rbf', gamma=0.7, C=C), svm.SVC(kernel='poly', degree=3, C=C))
+
 models = (clf.fit(x_train, y_train) for clf in models)
 score = clf.score(x_test, y_test)
-print(list(score))
+print(list(score))"""
 
-fig, sub = plt.subplots(2, 2)
-plt.subplots_adjust(wspace=0.4, hspace=0.4)
-
-xx, yy = make_meshgrid(x0, x1)
-
-titles = ('SVC with linear kernel',
-          'LinearSVC (linear kernel)',
-          'SVC with RBF kernel',
-          'SVC with polynomial (degree 3) kernel')
-
-for clf, title, ax in zip(models, titles, sub.flatten()):
-    plot_contours(ax, clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
-    ax.scatter(x0, x1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-    ax.set_xlim(xx.min(), xx.max())
-    ax.set_ylim(yy.min(), yy.max())
-    ax.set_xlabel('age')
-    ax.set_ylabel('freetime')
-    ax.set_xticks(())
-    ax.set_yticks(())
-    ax.set_title(title)
-plt.show()
